a8ecb3b7d7f0b1f6c1a2f50c2de7eaeb812dc5a3
STAWZW <1223868042@qq.com>
2021-09-21 23:02:38 +0800

    基础搭建

--- vue-tempalte/src/views/Home.vue
+++ vue-tempalte/src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-row>
+      <el-button @click="$router.push({ name: 'TableTemplate' })">表格模板</el-button>
+      <el-button type="primary">...</el-button>
+      <el-button type="success">...</el-button>
+      <el-button type="info">...</el-button>
+      <el-button type="warning">...</el-button>
+      <el-button type="danger">...</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button plain>...</el-button>
+      <el-button type="primary" plain>...</el-button>
+      <el-button type="success" plain>...</el-button>
+      <el-button type="info" plain>...</el-button>
+      <el-button type="warning" plain>...</el-button>
+      <el-button type="danger" plain>...</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button round>...</el-button>
+      <el-button type="primary" round>...</el-button>
+      <el-button type="success" round>...</el-button>
+      <el-button type="info" round>...</el-button>
+      <el-button type="warning" round>...</el-button>
+      <el-button type="danger" round>...</el-button>
+    </el-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
---
+++ vue-tempalte/src/views/table-template.vue
@@ -0,0 +1,48 @@
+<template>
+  <new-table :data="tableData" :columns="columns" @getRowData="test"></new-table>
+</template>
+<script>
+import NewTable from "@/components/newTable.vue";
+export default {
+  components: {
+    NewTable,
+  },
+
+  data() {
+    return {
+      columns: [
+        {label: "日期", dataIndex: "date", type: {name: 'date', value: ''}},
+        {label: "姓名", dataIndex: "name"},
+        {label: "地址", dataIndex: "address"},
+      ],
+
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    };
+  },
+
+  methods: {
+    test(val){
+      console.log(val)
+    }
+  }
+};
+</script>
+<style scoped>
+
+</style>
