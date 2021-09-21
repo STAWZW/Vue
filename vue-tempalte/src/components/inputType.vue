0d27d4b99fd111246e69b2fcf931a5de2aa5889c
STAWZW <1223868042@qq.com>
2021-09-21 23:03:27 +0800

    基础搭建

---
+++ vue-tempalte/src/components/inputType.vue
@@ -0,0 +1,36 @@
+<template>
+    <div v-if="type !== undefined">
+        <el-date-picker
+                v-model="date"
+                value-format="yyyy-MM-dd"
+                @change="(val) => $emit('input', val)"
+                type="date"
+                placeholder="选择日期">
+        </el-date-picker>
+    </div>
+    <div v-else>
+        <el-input :value="value" size="mini" @input="$emit('input', $event)"></el-input>
+    </div>
+</template>
+<script>
+
+    export default {
+
+        props: ['value', 'type'],
+
+        data() {
+            return {
+                date: this.value
+            }
+        },
+
+        methods: {
+            test(picker){
+                console.log(picker)
+            }
+        }
+    };
+</script>
+<style scoped>
+
+</style>
---
+++ vue-tempalte/src/layouts/BasicLayout.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-container>
+        <el-aside width="200px">
+            <Menu></Menu>
+        </el-aside>
+        <el-main>
+            <div :style="{ padding: '8px 16px', background: '#fff', minHeight: 'calc(100vh - 137px)', }">
+                <router-view></router-view>
+            </div>
+        </el-main>
+    </el-container>
+</template>
+<script>
+    import Menu from './Menu.vue'
+
+    export default {
+        components: {Menu},
+        data() {
+            return {
+                collapsed: false,
+            };
+        },
+        methods: {
+            onSearch() {
+            },
+        },
+    };
+</script>
+
+<style scoped>
+    #basic_layout .logo {
+        height: 31px;
+        background: rgba(255, 255, 255, 0.2);
+        margin: 16px;
+    }
+
+    #basic_layout .trigger {
+        font-size: 18px;
+        line-height: 55px;
+        padding: 0 24px;
+        cursor: pointer;
+        transition: color 0.3s;
+    }
+
+    #basic_layout .ant-breadcrumb {
+        line-height: 48px;
+    }
+
+    #basic_layout .trigger:hover {
+        color: #1890ff;
+    }
+</style>
